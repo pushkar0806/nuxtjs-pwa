d3f4634579af62872120cc0635a32914597e963e
pushkar0806 <pushkar_0806@hotmail.com>
2020-03-31 13:32:41 +0530

    pwa enabled nuxtjs app

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <section class="hero has-text-centered is-info">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">News Headlines</h1>
+          <h2 class="subtitle">Value beyond the knowledge</h2>
+        </div>
+      </div>
+    </section>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+</style>
+
